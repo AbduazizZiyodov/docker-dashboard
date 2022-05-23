b1c535fef6ddcd4aabdc0f9bf8361f7997af22ae
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-05-24 01:47:04 +0500

    :heavy_plus_sign: Add Image model utils

---
+++ src/server/images/utils.py
@@ -0,0 +1,32 @@
+import typing as t
+
+from docker import DockerClient
+from docker.models.images import Image
+
+
+def parse_image_name(image: Image) -> str:
+    return str(image).split(" ")[1]\
+        .split(":")[0] \
+        .replace("'", "")
+
+
+def image_as_dict(images: t.Union[t.List[Image], Image]):
+    def util(image: Image):
+        return dict(
+            id=image.id,
+            name=parse_image_name(image),
+            short_id=image.short_id,
+            labels=image.labels,
+
+        )
+
+    if isinstance(images, list):
+        return list(map(util, images))
+
+    return util(images)
+
+
+def remove_image(image: Image, client: DockerClient) -> None:
+    image_name: str = parse_image_name(image)
+    client.images.remove(image_name)
+    return
