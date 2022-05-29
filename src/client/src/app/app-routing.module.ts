6840c24bd2648888b10a03e51278d838408b29be
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-05-29 23:27:33 +0500

    :x: Create not found component for handling http 404

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './core/menu/menu.component';
 import { ContainersComponent } from './core/containers/containers.component';
+import { ImagesComponent } from './core/images/images.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: MenuComponent },
   { path: 'containers', component: ContainersComponent },
+  { path: 'images', component: ImagesComponent },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
