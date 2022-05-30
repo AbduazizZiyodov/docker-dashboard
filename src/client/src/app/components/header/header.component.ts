d9f4427401036e7c92ead14f8b4d48febe7b5ade
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-05-30 23:24:29 +0500

    :100: Add navigation button to header and icon to modal

--- src/client/src/app/components/header/header.component.ts
+++ src/client/src/app/components/header/header.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  constructor(private location: Location) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  back() {
+    this.location.back();
   }
-
+  next() {
+    this.location.forward();
+  }
 }
