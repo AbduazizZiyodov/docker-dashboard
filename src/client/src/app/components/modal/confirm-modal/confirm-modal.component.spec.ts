d17002dee6d4f92327f6f2297ed8fba97435d3c0
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-06-13 23:02:20 +0500

    :fire: Create modal for confirmation (delete)

---
+++ src/client/src/app/components/modal/confirm-modal/confirm-modal.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfirmModalComponent } from './confirm-modal.component';
+
+describe('ConfirmModalComponent', () => {
+  let component: ConfirmModalComponent;
+  let fixture: ComponentFixture<ConfirmModalComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ConfirmModalComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConfirmModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/client/src/app/components/modal/confirm-modal/confirm-modal.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input } from '@angular/core';
+import { ModalData } from '@models/modal';
+import { Image } from '@models/image';
+import { ImageService } from '@services/image.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { ContainerService } from '@services/container.service';
+
+@Component({
+  selector: 'app-confirm-modal',
+  templateUrl: './confirm-modal.component.html',
+  styleUrls: ['./confirm-modal.component.scss'],
+})
+export class ConfirmModalComponent {
+  @Input() data!: ModalData | undefined;
+
+  constructor(
+    private imageService: ImageService,
+    private toastr: ToastrService,
+    private router: Router,
+    private containerService: ContainerService
+  ) {}
+
+  delete() {
+    if (this.data?.is_delete_image_modal) {
+      this.deleteImage(this.data.resource);
+    } else if (this.data?.is_delete_container_modal) {
+      this.deleteContainer(this.data.resource?.id);
+    }
+  }
+
+  deleteContainer(container_id: string | any) {
+    return this.containerService
+      .deleteContainer(container_id)
+      .subscribe((res: any) => {
+        this.toastr.error(`Container ${container_id} deleted!`);
+        this.data?.modalRef?.close();
+        window.location.reload();
+      });
+  }
+
+  deleteImage(image: Image | any) {
+    this.imageService.deleteImage(image.short_id).subscribe((data) => {
+      this.toastr.error(`Image ${image.name} deleted!`);
+      this.router.navigate(['images']);
+      this.data?.modalRef?.close();
+    });
+  }
+}
