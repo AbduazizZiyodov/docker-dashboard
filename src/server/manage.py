281d5775beb2cca12e637bb39ea40dd194065593
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-06-19 20:45:48 +0500

    :recycle: Update requirements, add pytest config & cli command

--- src/server/manage.py
+++ src/server/manage.py
@@ -1,8 +1,11 @@
 import typer
+import pytest
 import uvicorn
 import typing as t
 
+from starlette.routing import Route
 from starlette.middleware import Middleware
+from starlette.responses import JSONResponse
 from starlette.applications import Starlette
 from starlette.exceptions import HTTPException
 from starlette.middleware.cors import CORSMiddleware
@@ -18,12 +21,15 @@ from exceptions import (
     http_exception_handler,
     pydantic_exception_handler
 )
+from tests.client import CustomAsyncTestClient
 
-cli = typer.Typer()
+cli, client = typer.Typer(), CustomAsyncTestClient()
 
 
 def create_app() -> Starlette:
-    routes = container_routes + image_routes
+    routes = container_routes + image_routes + [
+        Route("/", lambda _:JSONResponse("ok"), methods=["GET"])
+    ]
 
     return Starlette(
         routes=routes,
@@ -61,6 +67,11 @@ def serve(
         debug=debug,
         reload=reload
     )
+
+
+@cli.command()
+def run_tests():
+    pytest.main(["-s", "tests/"])
 
 
 if __name__ == "__main__":
