076987a595ad6f51c649aba159c12361937d65a1
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-06-21 19:59:16 +0500

    :tada: Major improvements on UI, created sidebar!

--- src/client/src/app/components/header/header.component.ts
+++ src/client/src/app/components/header/header.component.ts
@@ -1,18 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@env';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private location: Location) {}
+export class HeaderComponent implements OnInit {
+  isRunning: boolean = false;
+
+  constructor(private location: Location, private http: HttpClient) {}
 
   back() {
     this.location.back();
   }
   next() {
     this.location.forward();
+  }
+  ngOnInit(): void {
+    this.testAPI();
+  }
+
+  testAPI() {
+    this.http.get('http://127.0.0.1:8000').subscribe((res: any) => {
+      if (res == 'ok') {
+        this.isRunning = true;
+      }
+    });
   }
 }
