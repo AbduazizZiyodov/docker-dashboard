92e3a1150e36a77e690e99ba48ca85c700216a1a
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-07-11 16:08:00 +0500

    :100: Add apt check for remove

--- src/client/build_linux.sh
+++ src/client/build_linux.sh
@@ -1,16 +1,20 @@
-sudo apt-get remove docker-dashboard -y
+PACKAGE="docker-dashboard"
+STATUS=$(dpkg-query -W --showformat='${Status}\n' $PACKAGE | grep "install ok installed")
+
+if [ "install ok installed" = "$STATUS" ]; then
+  sudo apt-get remove $PACKAGE -y
+fi
+
 rm -rf builds/
 
 ng build
 
 cp package.json builds/docker-dashboard-ng-build/ &&
-    cp src/main.js builds/docker-dashboard-ng-build/
+  cp src/main.js builds/docker-dashboard-ng-build/
 
 electron-packager builds/docker-dashboard-ng-build docker-dashboard --overwrite --asar \
-    --platform=linux --arch=x64 \
-    --prune=true --icon=src/icon.ico \
-    --out=builds/
+  --platform=linux --arch=x64 \
+  --prune=true --icon=src/icon.ico \
+  --out=builds/
 
 node installer.js
-
-sudo dpkg -i builds/docker-dashboard_1.0.0_amd64.deb
