662355c43522e1ca11fd82563a53b756fe42baf1
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-07-26 02:27:03 +0500

    :pencil: Updated schemas. added tag field to pull req.

--- src/server/images/schemas.py
+++ src/server/images/schemas.py
@@ -4,10 +4,9 @@ from pydantic import BaseModel
 
 class DockerSearchRequest(BaseModel):
     term: str
-    limit: Optional[str]
+    limit: Optional[int] = 10
 
 
 class DockerPullRequest(BaseModel):
-    username: str
-    password: str
     repository: str
+    tag: Optional[str] = "latest"
