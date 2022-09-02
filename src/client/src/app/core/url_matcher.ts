1f167712e3a5c6b3734585fb36a81ef677a46431
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-09-03 01:23:27 +0500

    :pencil: rename utils to url_matcher

--- src/client/src/app/core/url_matcher.ts
+++ src/client/src/app/core/url_matcher.ts
@@ -1,11 +1,6 @@
 import { UrlMatchResult, UrlSegment } from '@angular/router';
 
 export function repositoryUrlMatcher(url: UrlSegment[]): UrlMatchResult | null {
-  /*
-    Match repository
-        pull/nginx <-- nginx is repo
-        pull/dpage/pgadmin <-- dpage/pgadmin is also repo not ROUTE!
-  */
   const result: UrlMatchResult = {
     consumed: url,
     posParams: {},
@@ -14,7 +9,6 @@ export function repositoryUrlMatcher(url: UrlSegment[]): UrlMatchResult | null {
 
   if (url[0].path == 'pull' && url.length > 1) {
     if (url.length == 2) {
-      // like -> pull/nginx
       result.posParams = {
         repository: new UrlSegment(url[1].path, {}),
       };
@@ -22,7 +16,6 @@ export function repositoryUrlMatcher(url: UrlSegment[]): UrlMatchResult | null {
     }
 
     if (url.length == 3) {
-      // like -> pull/dpage/pgadmin
       result.posParams = {
         repository: new UrlSegment(`${url[1].path}/${url[2].path}`, {}),
       };
