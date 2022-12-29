4234ce8dc6266023b8ac1cc7f90e0aefb6b10165
Abduaziz <abduaziz.ziyodov@mail.ru>
2022-12-30 01:59:59 +0500

    Fix: validate ws request for incorrect validation

--- src/server/utils/websocket.py
+++ src/server/utils/websocket.py
@@ -2,19 +2,22 @@ import json
 import typing as t
 from pydantic import ValidationError, BaseModel
 
-from server.schemas import DockerPullRequest
+from server.models import DockerPullRequest
 
 NoneType = type(None)
 
 
 async def validate_websocket_request(data: dict, PydanticModel: BaseModel = DockerPullRequest) -> \
         t.Tuple[BaseModel, t.Union[NoneType, t.Dict[str, dict]]]:
+    """Util function for validating websocket requests via pydantic models.
+    """
     try:
-        body = PydanticModel(**data)
+        if "DockerPullRequest" in str(PydanticModel):
+            if data.get("action") == "list":
+                data["repository"] = ""
 
-        if body.__repr_name__ == "DockerPullRequest":
-            if body.action == "create" and len(body.repository) == 0:
-                return None, {"detail": "Repository can't be None if action == create"}
+        body = PydanticModel(**data)
         return body, None
+        
     except ValidationError as exc:
         return None, {"detail": json.loads(exc.json())}
