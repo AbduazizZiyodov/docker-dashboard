8baf4aef497bf3a077892163dd3a5d60cf382447
Abduaziz <abduaziz.ziyodov@mail.ru>
2023-04-29 03:47:02 +0500

    Removed tasks, added websocket req. handler

--- src/server/asgi.py
+++ src/server/asgi.py
@@ -1,5 +1,9 @@
 from starlette.applications import Starlette
 from server.settings import STARLETTE_SETTINGS
+from server.exceptions import exception_handlers
 
 
 application = Starlette(**STARLETTE_SETTINGS)
+
+for exception_class, handler in exception_handlers.items():
+    application.add_exception_handler(exception_class, handler)
--- src/server/exceptions.py
+++ src/server/exceptions.py
@@ -1,6 +1,11 @@
 import json
-from pydantic import ValidationError
 
+from typing import Any
+from pydantic import BaseModel
+from pydantic import ValidationError
+from docker.errors import DockerException
+
+import starlette.status as error
 from starlette.responses import JSONResponse
 from starlette.exceptions import HTTPException
 
@@ -8,8 +13,7 @@ from server.types import HttpDockerException
 
 
 async def http_exception_handler(_, exc: HttpDockerException) -> JSONResponse:
-    """Starlette HTTP exception handler
-    """
+    """Starlette HTTP exception handler"""
     detail = exc.detail if isinstance(exc, HTTPException) else exc.explanation
 
     return JSONResponse(
@@ -20,10 +24,20 @@ async def http_exception_handler(_, exc: HttpDockerException) -> JSONResponse:
 
 
 async def pydantic_exception_handler(_, exc: ValidationError) -> JSONResponse:
-    """Handling pydantic validation exceptions
-    """
+    """Handling pydantic validation exceptions"""
     detail: dict = json.loads(exc.json())
-    return JSONResponse(detail, status_code=422)
+    return JSONResponse(detail, status_code=error.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
-__all__ = ["pydantic_exception_handler", "http_exception_handler"]
+def websocket_request_handler(data: Any, Model: BaseModel) -> Any:
+    try:
+        return Model(**data), None
+    except ValidationError as exc:
+        return None, json.loads(exc.json())
+
+
+exception_handlers = {
+    HTTPException: http_exception_handler,
+    DockerException: http_exception_handler,
+    ValidationError: pydantic_exception_handler,
+}
